364b5118959d25863cc39cf661d3e0ebeb62c4ff
Szymon Stanisław Grzesiak <szymongrzesiak.pl@gmail.com>
2024-08-18 20:09:55 +0200

    small fix

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,66 +1,69 @@
-import { ImageResponse } from 'next/og';
-import { NextRequest } from 'next/server';
+import { ImageResponse } from "next/og";
+import { NextRequest } from "next/server";
 
-export const runtime = "edge"; 
+export const runtime = "edge";
 
 const font = fetch(
-  new URL('../../../public/fonts/kaisei-tokumin-bold.ttf', import.meta.url)
+  new URL("../../../public/fonts/kaisei-tokumin-bold.ttf", import.meta.url)
 ).then((res) => res.arrayBuffer());
 
 export async function GET(req: NextRequest) {
-  const { searchParams } = req.nextUrl;
-  const postTitle = searchParams.get('title');
- 
-  const fontData = await font;
+  try {
+    const fontData = await font;
+    const { searchParams } = req.nextUrl;
+    const postTitle = searchParams.get("title");
 
-  if (!postTitle) {
-    return new Response("No title provided", { status: 500 });
-  }
+    if (!postTitle) {
+      return new Response("No title provided", { status: 500 });
+    }
 
-  const heading =
+    const heading =
       postTitle.length > 140 ? `${postTitle.substring(0, 140)}...` : postTitle;
 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          height: '100%',
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'flex-start',
-          justifyContent: 'center',
-          backgroundImage: 'url(https://szymongrzesiak.dev/og-bg.png)',
-        }}
-      >
+    return new ImageResponse(
+      (
         <div
           style={{
-            marginLeft: 190,
-            marginRight: 190,
-            display: 'flex',
-            fontSize: 130,
-            fontFamily: 'Kaisei Tokumin',
-            letterSpacing: '-0.05em',
-            fontStyle: 'normal',
-            color: 'white',
-            lineHeight: '120px',
-            whiteSpace: 'pre-wrap',
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            justifyContent: "center",
+            backgroundImage: "url(https://szymongrzesiak.dev/og-bg.png)",
           }}
         >
-          {heading}
+          <div
+            style={{
+              marginLeft: 190,
+              marginRight: 190,
+              display: "flex",
+              fontSize: 130,
+              fontFamily: "Kaisei Tokumin",
+              letterSpacing: "-0.05em",
+              fontStyle: "normal",
+              color: "white",
+              lineHeight: "120px",
+              whiteSpace: "pre-wrap",
+            }}
+          >
+            {heading}
+          </div>
         </div>
-      </div>
-    ),
-    {
-      width: 1920,
-      height: 1080,
-      fonts: [
-        {
-          name: 'Kaisei Tokumin',
-          data: fontData,
-          style: 'normal',
-        },
-      ],
-    }
-  );
+      ),
+      {
+        width: 1920,
+        height: 1080,
+        fonts: [
+          {
+            name: "Kaisei Tokumin",
+            data: fontData,
+            style: "normal",
+          },
+        ],
+      }
+    );
+  } catch (error) {
+    return new Response("Failed to generate image", { status: 500 });
+  }
 }
