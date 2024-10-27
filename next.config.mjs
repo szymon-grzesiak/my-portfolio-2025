9d8ba118f9fabb6dbe068b457e5ee821753e2174
Szymon Stanisław Grzesiak <szymongrzesiak.pl@gmail.com>
2024-10-27 23:37:43 +0100

    vercel fixes

--- next.config.mjs
+++ next.config.mjs
@@ -35,14 +35,13 @@ const nextConfig = {
 const ContentSecurityPolicy = `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline' data: https://www.googletagmanager.com https://www.google-analytics.com https://www.google.com https://www.gstatic.com https://assets.aceternity.com https://va.vercel-scripts.com https://vercel.live;
-    style-src 'self' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline' https://vercel.live;
     img-src * blob: data: https://assets.aceternity.com;
     media-src 'none';
     connect-src *;
     font-src 'self' data: https://fonts.googleapis.com https://fonts.gstatic.com https://use.typekit.net https://p.typekit.net https://fonts.google.com/;
     frame-src 'self' https://vercel.live;
 `;
-
 const securityHeaders = [
   {
     key: "Content-Security-Policy",
