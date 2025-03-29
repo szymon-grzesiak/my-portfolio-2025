698270d817e98bc5014f0d7bb2c5ac843290116c
Szymon Grzesiak <szymongrzesiak.pl@gmail.com>
2025-03-29 20:50:02 +0100

    fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ import { posts } from "#site/content";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   let blogs = posts.map((post) => ({
-    url: `https://szymongrzesiak.dev/blog/${post.slug}`,
+    url: `https://szymongrzesiak.dev/${post.slug}`,
     lastModified: post.date,
   }));
 
