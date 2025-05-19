719ab54a6ce2ffcb176553cd8986eb8d5b7ae4ee
Szymon <szymongrzesiak.pl@gmail.com>
2025-05-19 21:19:38 +0200

    fix

--- app/robots.ts
+++ app/robots.ts
@@ -6,12 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: [
-          "/_next/",
-          "/api/",
-          "/static/",
-          "/*?_rsc=*",
-        ],
       },
     ],
     sitemap: "https://szymongrzesiak.dev/sitemap.xml",
--- next.config.mjs
+++ next.config.mjs
@@ -28,63 +28,63 @@ const nextConfig = {
     config.plugins.push(new VeliteWebpackPlugin());
     return config;
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://*.google-analytics.com https://www.google.com https://www.gstatic.com https://assets.aceternity.com https://*.vercel-scripts.com https://*.vercel.com;
-              style-src 'self' 'unsafe-inline' https://vercel.live;
-              img-src 'self' data: blob: https://assets.aceternity.com https://*.google-analytics.com;
-              media-src 'none';
-              connect-src 'self' https://www.googletagmanager.com https://*.google-analytics.com https://www.google.com https://*.vercel.com;
-              font-src 'self' data: https://fonts.googleapis.com https://fonts.gstatic.com https://use.typekit.net https://p.typekit.net https://fonts.google.com/;
-              frame-src 'self' https://vercel.live;
-              object-src 'none';
-              base-uri 'self';
-              form-action 'self';
-              frame-ancestors 'none';
-              block-all-mixed-content;
-              upgrade-insecure-requests;
-            `
-              .replace(/\s+/g, " ")
-              .trim(),
-          },
-          {
-            key: "X-DNS-Prefetch-Control",
-            value: "on",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "origin-when-cross-origin",
-          },
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload",
-          },
-          {
-            key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=()",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         {
+  //           key: "Content-Security-Policy",
+  //           value: `
+  //             default-src 'self';
+  //             script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://*.google-analytics.com https://www.google.com https://www.gstatic.com https://assets.aceternity.com https://*.vercel-scripts.com https://*.vercel.com;
+  //             style-src 'self' 'unsafe-inline' https://vercel.live;
+  //             img-src 'self' data: blob: https://assets.aceternity.com https://*.google-analytics.com;
+  //             media-src 'none';
+  //             connect-src 'self' https://www.googletagmanager.com https://*.google-analytics.com https://www.google.com https://*.vercel.com;
+  //             font-src 'self' data: https://fonts.googleapis.com https://fonts.gstatic.com https://use.typekit.net https://p.typekit.net https://fonts.google.com/;
+  //             frame-src 'self' https://vercel.live;
+  //             object-src 'none';
+  //             base-uri 'self';
+  //             form-action 'self';
+  //             frame-ancestors 'none';
+  //             block-all-mixed-content;
+  //             upgrade-insecure-requests;
+  //           `
+  //             .replace(/\s+/g, " ")
+  //             .trim(),
+  //         },
+  //         {
+  //           key: "X-DNS-Prefetch-Control",
+  //           value: "on",
+  //         },
+  //         {
+  //           key: "X-Frame-Options",
+  //           value: "DENY",
+  //         },
+  //         {
+  //           key: "X-Content-Type-Options",
+  //           value: "nosniff",
+  //         },
+  //         {
+  //           key: "X-XSS-Protection",
+  //           value: "1; mode=block",
+  //         },
+  //         {
+  //           key: "Referrer-Policy",
+  //           value: "origin-when-cross-origin",
+  //         },
+  //         {
+  //           key: "Strict-Transport-Security",
+  //           value: "max-age=63072000; includeSubDomains; preload",
+  //         },
+  //         {
+  //           key: "Permissions-Policy",
+  //           value: "camera=(), microphone=(), geolocation=()",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 export default nextConfig;
