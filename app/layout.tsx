62bda540a01715cf4ecc6f19e348d6b96498dd7e
Szymon <szymongrzesiak.pl@gmail.com>
2025-05-21 22:19:59 +0200

    maybe

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,7 +90,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${poppins.variable} ${jersey.variable} ${bagel.variable} font-sans antialiased text-black bg-white dark:text-white dark:bg-[#111110]`}
+      className={`${poppins.variable} ${jersey.variable} ${bagel.variable} font-sans antialiased text-black bg-white`}
       suppressHydrationWarning
     >
       <head>
